50c3163fa0a83fe40a7b2802c5a534e7acf75538
OrdinaryGuy <git@ordinaryguy.me>
2015-03-28 21:55:27 +0300

    Adding Font Awesome & shrinking style.css

--- functions.php
+++ functions.php
@@ -2,10 +2,8 @@
 
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
+    wp_enqueue_style( 'child-style',  get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+    wp_enqueue_style( 'child-style-fa', get_stylesheet_directory_uri() . '/inc/fontawesome/css/font-awesome.min.css');
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
