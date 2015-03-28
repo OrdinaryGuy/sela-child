a19bafb12fd7bf543fd17ad0572804fb50818595
OrdinaryGuy <git@ordinaryguy.me>
2015-03-28 18:00:15 +0300

    Initial commit

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function sela_child_theme_setup() {
+	load_child_theme_textdomain( 'sela-child', get_template_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'sela_child_theme_setup' );
+
+/* Enabling shortcode processing for widgets */
+add_filter('widget_text', 'do_shortcode');
+
+?>
